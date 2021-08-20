3e3430c31f235eb082548cf88c7a8d618bb263bd
JacintaO <85346925+JacintaO@users.noreply.github.com>
2021-08-20 16:33:04 +0100

    Add files via upload

---
+++ indexII.js
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>JACINTA AMAKA ONYEODILI</title>
+
+    <style>
+
+body{
+
+    background-color: black);
+
+}
+
+    </style>
+
+</head>
+
+<body>
+
+<div>
+
+    <a href="https://internship.zuri.team/"><img src="./Images/HNG_LOGO-.png" alt="HNG Logo" width=20% height=50%></a>
+
+</div>
+
+
+
+    <script  language="javascript" type="text/javascript">document.write("<h1>Otuokere Chibuchi Elizabeth</h1>");
+
+    </script>
+
+</body>
+
+</html>
